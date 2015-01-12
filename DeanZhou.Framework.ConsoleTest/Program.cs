2bc2bbd1daf2fdc196ab5dc71a018f84f94c64e4
DeanZhouLin <funny_zhoulin@163.com>
2015-01-12 21:14:46 +0800

    提交1

--- DeanZhou.Framework.ConsoleTest/Program.cs
+++ DeanZhou.Framework.ConsoleTest/Program.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace DeanZhou.Framework.ConsoleTest
 {
---
+++ DeanZhou.Framework/Apps/ORMFramework/DapperHelper.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DeanZhou.Framework.Apps.ORMFramework
+{
+    public sealed class DapperHelper
+    {
+        private static readonly object LockObj;
+        private static readonly Dictionary<string, DapperHelper> DapperHelpers;
+
+        private readonly string _connStr;
+
+        static DapperHelper()
+        {
+            DapperHelpers = new Dictionary<string, DapperHelper>();
+            LockObj = new object();
+        }
+
+        private DapperHelper(string connStr)
+        {
+            _connStr = connStr;
+        }
+
+        public static DapperHelper GetInstance(string connStr)
+        {
+            lock (LockObj)
+            {
+                if (DapperHelpers.ContainsKey(connStr))
+                {
+                    return DapperHelpers[connStr];
+                }
+                DapperHelper dpRes = new DapperHelper(connStr);
+                DapperHelpers.Add(connStr, dpRes);
+                return dpRes;
+            }
+        }
+
+        private IDbConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(_connStr);
+            connection.Open();
+            return connection;
+        }
+
+        public IEnumerable<T> Query<T>(string execSql)
+        {
+            using (IDbConnection con = OpenConnection())
+            {
+                return con.Query<T>(execSql);
+            }
+        }
+
+        public object Execute<T>(string execSql, T t)
+        {
+            using (IDbConnection con = OpenConnection())
+            {
+                return con.Execute(execSql, t);
+            }
+        }
+    }
+
+}
