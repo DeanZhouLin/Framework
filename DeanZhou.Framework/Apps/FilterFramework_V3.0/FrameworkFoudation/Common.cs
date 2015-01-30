ed605a0a0aaf67f458ec0e912ea06a5016e5476f
DeanZhouLin <funny_zhoulin@163.com>
2015-01-30 17:04:04 +0800

    添加通用方法

---
+++ DeanZhou.Framework/Apps/FilterFramework_V3.0/FrameworkFoudation/Common.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Reflection;
+
+namespace DeanZhou.Framework
+{
+    public static class Common
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TItemType"></typeparam>
+        /// <typeparam name="TEnumType"></typeparam>
+        /// <param name="assemblyName"></param>
+        /// <param name="fullClassName"></param>
+        /// <returns></returns>
+        public static IEnumTypeIdentifier<TItemType, TEnumType> CreateIdentifier<TItemType, TEnumType>(string assemblyName, string fullClassName)
+            where TItemType : class
+        {
+            var ect = CreateInstance(assemblyName, fullClassName) as IEnumTypeIdentifier<TItemType, TEnumType>;
+            return ect;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TItemType"></typeparam>
+        /// <typeparam name="TParamType"></typeparam>
+        /// <typeparam name="TEnumType"></typeparam>
+        /// <param name="assemblyName"></param>
+        /// <param name="fullClassName"></param>
+        /// <returns></returns>
+        public static IEnumTypeIdentifier<TItemType, TParamType, TEnumType> CreateIdentifier<TItemType, TParamType, TEnumType>(string assemblyName, string fullClassName)
+            where TItemType : class
+            where TParamType : class
+        {
+            var ect = CreateInstance(assemblyName, fullClassName) as IEnumTypeIdentifier<TItemType, TParamType, TEnumType>;
+            return ect;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TItemType"></typeparam>
+        /// <param name="assemblyName"></param>
+        /// <param name="fullClassName"></param>
+        /// <returns></returns>
+        public static IFilter<TItemType> CreateIFilter<TItemType>(string assemblyName, string fullClassName)
+            where TItemType : class
+        {
+            var ect = CreateInstance(assemblyName, fullClassName) as IFilter<TItemType>;
+            return ect;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TItemType"></typeparam>
+        /// <typeparam name="TParamType"></typeparam>
+        /// <param name="assemblyName"></param>
+        /// <param name="fullClassName"></param>
+        /// <returns></returns>
+        public static IFilter<TItemType, TParamType> CreateIFilter<TItemType, TParamType>(string assemblyName, string fullClassName)
+            where TItemType : class
+            where TParamType : class
+        {
+            var ect = CreateInstance(assemblyName, fullClassName) as IFilter<TItemType, TParamType>;
+            return ect;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="assemblyName"></param>
+        /// <param name="fullClassName"></param>
+        /// <returns></returns>
+        public static object CreateInstance(string assemblyName, string fullClassName)
+        {
+            return Assembly.Load(assemblyName).CreateInstance(fullClassName);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumStr"></param>
+        /// <returns></returns>
+        public static T CreateEnum<T>(string enumStr)
+        {
+            return (T)Enum.Parse(typeof(T), enumStr, true);
+        }
+
+    }
+}
